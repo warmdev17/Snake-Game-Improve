d6453457a115698956c61cbe600a0eaa3cad43a6
warmdev17 <warmdevofficial@gmail.com>
2025-01-12 12:13:41 +0700

    user select none

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import SnakeGame from "@/components/SnakeGame";
 
 const Home: React.FC = () => {
   return (
-    <div className="flex justify-center bg-[#4a752c] items-center h-screen flex-col">
+    <div className="flex justify-center bg-[#4a752c] items-center h-screen flex-col select-none">
       <SnakeGame />
     </div>
   );
